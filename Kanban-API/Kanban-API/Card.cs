fb197b7141a0d01c0ab18d6c3620ee45129dc2ca
emaraan <emaraan@gmail.com>
2016-02-02 16:35:02 -0800

    added card resource

--- Kanban-API/Kanban-API/Card.cs
+++ Kanban-API/Kanban-API/Card.cs
@@ -9,16 +9,38 @@
 
 namespace Kanban_API
 {
+    using Models;
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
     public partial class Card
     {
+        public Card()
+        {
+            this.CardId = CardId;
+            this.CreatedDate = CreatedDate;
+            this.ListId = ListId;
+            this.Text = Text;
+        }
+
+        public Card(CardModel model)
+        {
+            this.Update(model);
+            CreatedDate = DateTime.Now;
+        }
+
         public int CardId { get; set; }
         public int ListId { get; set; }
         public System.DateTime CreatedDate { get; set; }
+
+        [Required]
         public string Text { get; set; }
     
         public virtual List List { get; set; }
+
+        public void Update(CardModel model)
+        {
+            Text = model.Text;
+        }
     }
 }
---
+++ Kanban-API/Kanban-API/Controllers/CardsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Kanban_API;
+using AutoMapper;
+using Kanban_API.Models;
+
+namespace Kanban_API.Controllers
+{
+    public class CardsController : ApiController
+    {
+        private KanbanEntities db = new KanbanEntities();
+
+        // GET: api/Cards/5
+        [ResponseType(typeof(CardModel))]
+        public IHttpActionResult GetCard(int id)
+        {
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<CardModel>(card));
+        }
+
+        // PUT: api/Cards/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCard(int id, CardModel card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != card.CardId)
+            {
+                return BadRequest();
+            }
+
+            var dbCard = db.Cards.Find(id);
+
+            dbCard.Update(card);
+
+            db.Entry(dbCard).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cards
+        [ResponseType(typeof(CardModel))]
+        public IHttpActionResult PostCard(CardModel card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var dbCard = new Card(card);
+            db.Cards.Add(dbCard);
+            db.SaveChanges();
+
+            card.CreatedDate = dbCard.CreatedDate;
+            card.CardId = dbCard.CardId;
+
+            return CreatedAtRoute("DefaultApi", new { id = dbCard.CardId }, Mapper.Map<CardModel>(card));
+        }
+
+        // DELETE: api/Cards/5
+        [ResponseType(typeof(Card))]
+        public IHttpActionResult DeleteCard(int id)
+        {
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            db.Cards.Remove(card);
+            db.SaveChanges();
+
+            return Ok(Mapper.Map<CardModel>(card));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CardExists(int id)
+        {
+            return db.Cards.Count(e => e.CardId == id) > 0;
+        }
+    }
+}
