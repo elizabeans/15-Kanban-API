fc5a23b4aca9ef9f59911a723448dd000f9f0d46
emaraan <emaraan@gmail.com>
2016-02-04 16:15:30 -0800

    initial commit, added lists and cards

---
+++ Kanban.Web/js/app.js
@@ -0,0 +1,45 @@
+﻿// The Angular Module
+angular.module('kanban', ['ngResource']);
+
+angular.module('kanban').value('apiUrl', 'http://localhost:64124/api');
+// The Controller
+angular.module('kanban').controller('IndexController', function ($scope, $resource, apiUrl) {
+
+    var ListResource = $resource(apiUrl + '/lists/:listId', { listId: '@id' },
+        {
+            'cards': {
+                url: apiUrl + '/lists/:listId/cards',
+                method: 'GET',
+                isArray: true
+            }
+        });
+    
+
+    function activate() {
+        ListResource.query(function (data) {
+            $scope.lists = data;
+            $scope.lists.forEach(function (list) {
+                list.cards = ListResource.cards({ listId: list.ListId });
+            });
+        });
+    }
+
+    $scope.newList = {};
+
+    $scope.addList = function () {
+        ListResource.save($scope.newList, function () {
+            alert('New list added!');
+            activate();
+        });
+    };
+
+    $scope.deleteList = function (list) {
+        $scope.currentList = list;
+        ListResource.remove($scope.currentList, function () {
+            alert('List deleted!');
+            activate();
+        });
+    };
+
+    activate();
+});
